9240d837ae87c1f265749c711775ac5eb1d93a62
Jota Santos <jamillo@gmail.com>
2020-10-06 02:06:31 -0300

    ✨ Implement the concrete Listener and MockListener (#5)
    Fix #2

---
+++ impl.go
@@ -0,0 +1,33 @@
+package signals
+
+import (
+	"os"
+	"os/signal"
+)
+
+type listener struct {
+	signals []os.Signal
+	sigCh   chan os.Signal
+}
+
+// NewListener returns the default implementation of a `Listener`.
+func NewListener(signals ...os.Signal) Listener {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, signals...)
+	return &listener{
+		signals: signals,
+		sigCh:   ch,
+	}
+}
+
+// Receive returns the receive only channel from where the signals will be written
+// to.
+func (l *listener) Receive() <-chan os.Signal {
+	return l.sigCh
+}
+
+// Stop stops listening the signal.
+func (l *listener) Stop() {
+	close(l.sigCh)
+	signal.Stop(l.sigCh)
+}
